7a23a57a62531a79c42e5a3700b79dd775d3d474
chanpark036 <cpark036@gmail.com>
2023-05-23 21:41:51 +0900

    fixed small issue with naming a file for deployment

--- Personal Website/Experience/Experience.js
+++ Personal Website/Experience/Experience.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import Sizes from "./Utils/Sizes.js"
 import Time from "./Utils/Time.js"
 import Resources from "./Utils/Resources.js"
-import assets from "./Utils/Assets.js";
+import assets from "./Utils/assets.js";
 
 import Camera from "./Camera.js"
 import Theme from "./Theme.js"
